bde31a5368599fd90b4fa02977e466bc81db2280
Simon Caron <git@simoncaron.com>
2024-05-25 23:04:16 -0400

    Remove duplicate provider version

--- modules/config/provider.tf
+++ modules/config/provider.tf
@@ -2,7 +2,6 @@ terraform {
   required_providers {
     proxmox = {
       source                = "bpg/proxmox"
-      version               = "0.57.1"
       configuration_aliases = [proxmox.pve_cluster]
     }
   }
